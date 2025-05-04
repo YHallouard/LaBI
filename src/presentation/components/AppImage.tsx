99c47334e3ccae8dd7a79213dd60db23926ed1e5
Yann HALLOUARD <yann.hal@gmail.com>
2025-05-04 18:04:01 +0200

    fix

--- src/presentation/components/AppImage.tsx
+++ src/presentation/components/AppImage.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Image, ImageProps, ImageSourcePropType } from "react-native";
 
-// Import images directly - ESLint ignore only for this specific import
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const adaptiveIcon = require("../../../assets/adaptive-icon.png");
 
@@ -9,7 +8,6 @@ type AppImageProps = Omit<ImageProps, "source"> & {
   imagePath: "adaptive-icon";
 };
 
-// Pre-define the image sources
 const imageSources: Record<string, ImageSourcePropType> = {
   "adaptive-icon": adaptiveIcon,
 };
